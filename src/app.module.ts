d5b036e1cf7e29c14416e3f2d49e3169d212c5cc
@stillascold <stillascold@gmail.com>
2024-10-02 00:37:57 -0400

    Created image service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImageService } from './image/image.service';
+import { ImageController } from './image/image.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ImageController],
+  providers: [AppService, ImageService],
 })
 export class AppModule {}
